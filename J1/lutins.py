e46f92f9e2f97658b8929be05e1cb3d7ef5e004c
Nicolas Delahaie <nico601.delahaie@gmail.com>
2022-12-10 01:28:23 +0100

    Etoiles 1 et 2

---
+++ J1/lutins.py
@@ -0,0 +1,20 @@
+﻿
+fic = open("messageADecoder.txt", "r")
+datas = fic.read()
+fic.close()
+
+valeurs=datas.split("\n\n")
+for n in range(len(valeurs)):
+    valeurs[n] = valeurs[n].split("\n")
+for lutin in range(len(valeurs)):
+    for aliment in range(len(valeurs[lutin])):
+        valeurs[lutin][aliment] = int(valeurs[lutin][aliment])
+for lutin in range(len(valeurs)):
+    somme = 0
+    for aliment in range(len(valeurs[lutin])):
+        somme += valeurs[lutin][aliment]
+    valeurs[lutin] = somme
+valeurs.sort()
+
+print (valeurs[-1]+valeurs[-2]+valeurs[-3])
+

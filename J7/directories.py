f440dc2c0b368dbe7c2d5ddc80b87f0f6fdc81af
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-12-16 17:22:58 +0100

    Amelioration de toString

--- J7/directories.py
+++ J7/directories.py
@@ -1,4 +1,4 @@
-fic = open("msgTest.txt", "r")
+fic = open("msg.txt", "r")
 datas = fic.read()
 fic.close()
 
@@ -9,9 +9,9 @@ lignesConsole.remove("$ cd /")
 
 class Dossier:
     """Definition d un dossier"""
-    def __init__(self, nom, contenu=[]):
+    def __init__(self, nom:str, contenu = None):
         self._nom = nom
-        self._contenu = contenu
+        self._contenu = contenu if contenu else []
     
     def getSousDossier(self, arborescence=[]):
         if (arborescence == []):
@@ -33,38 +33,52 @@ class Dossier:
     def ajouter (self, elmt):
         self._contenu.append(elmt)
         
-        
-        
-    def sommeFichiers (self):
+    def sommeFichiers (self, tailleMax = 100000):
         somme = 0
-        
         for enfant in self._contenu:
             if isinstance(enfant, Dossier):
                 #Dossier
                 somme += enfant.sommeFichiers()
             else:
                 #Condition d arret : ne pas etre un dossier
-                somme += enfant
-        
+                if enfant < tailleMax:
+                    somme += enfant
         return somme
-    def toString (self):
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def __str__ (self, profondeur = 0):
         '''Retourne le dossier de la maniere suivante :
             doss(doss1(), doss2(11), 11, doss3(doss4(11,11), doss5()))'''
-        msg = self._nom+" ("
+        
+        
+        msg = "- "+self._nom+"\n"
+        
+        profondeur += 1
         for enfant in self._contenu:
+            for loop in range(profondeur):
+                msg += "  "
             if isinstance(enfant, Dossier):
                 #Dossier
-                msg += enfant.toString()
+                msg += enfant.__str__(profondeur)
             else:
                 #Condition d arret : ne pas etre un dossier
-                msg += str(enfant)
-            if not self._contenu.index(enfant) == len(self._contenu)-1:
-                #Si c est le dernier element
-                msg += ", "
-        msg += ")"
+                msg += "- "+str(enfant)+"\n"
+
+
         return msg
 
+"""
 
+- source
+  - a
+    - e
+      827749
+  - b
+  - c
+
+"""
 source = Dossier("source", [
             Dossier("a", [
                 Dossier("e",[
@@ -81,21 +95,22 @@ source = Dossier("source", [
                 7214296])])
 
 
-source.getSousDossier(["d"]).ajouter(Dossier("x", []))
-source.getSousDossier(["d", "x"]).ajouter(999)
-print(source.getSousDossier(["d","x"])._contenu)
+#source.getSousDossier(["d"]).ajouter(Dossier("x", []))
+#source.getSousDossier(["d", "x"]).ajouter(999)
+#print(source.getSousDossier(["d","x"])._contenu)
 #print("somme =",source.sommeFichiers())
 
-
+#print(source.sommeFichiers())
+"""
 lignesConsole=['$ ls',
  'dir a',
  '14848514 b.txt',
  'dir d',
  '$ cd a']
  #'29116 f']
-
+"""
 source = Dossier("source")
-print(source.toString())
+#print(source.toString())
 
 emplacementCourant = []
 for ligne in lignesConsole:
@@ -105,25 +120,28 @@ for ligne in lignesConsole:
         if (ligne[-2:] == ".."):
             #cd Retour
             emplacementCourant.pop()
-            print("Retour")
+            #print("Retour")
             
         else:
             #cd Repertoire
             emplacementCourant.append(ligne[5:])
-            print("Déplace :",ligne[5:])
+            #print("Déplace :",ligne[5:])
         
     elif (ligne[:3] == "dir"):
         #Creer repertoire
         source.getSousDossier(emplacementCourant).ajouter(Dossier(ligne[4:]))
-        print("Créé :",ligne[4:],"dans",emplacementCourant)
+        #print("Créé :",ligne[4:],"dans",emplacementCourant)
         
     else:
         if (ligne[:4] != "$ ls"):
             #Creer fichier
             tailleFic = int(ligne.split(" ")[0])
-            print("Créé :", tailleFic,"dans",emplacementCourant,", le fichier avec comme contenu : ",source.getSousDossier(emplacementCourant)._contenu)
+            #print("Créé :", tailleFic,"dans",emplacementCourant,", le fichier avec comme contenu : ",source.getSousDossier(emplacementCourant)._contenu)
+            #print("Créé :",tailleFic,"dans",emplacementCourant)
             source.getSousDossier(emplacementCourant).ajouter(tailleFic)
             
+print(source)
+print(source.sommeFichiers())
     
 print("\n\n")
 
